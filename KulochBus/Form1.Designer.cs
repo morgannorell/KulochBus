4266a078a0b3708457f591620d7659a0adde79c0
morgannorell <mnorell@live.se>
2016-05-17 16:21:14 +0200

    Skapat ny applikation

---
+++ KulochBus/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace KulochBus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arkivToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.närvaroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.medlemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kontaktToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redigeraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.visaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.närvaroToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.medlemmarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hjälpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.visaHjälpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.omToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arkivToolStripMenuItem,
+            this.redigeraToolStripMenuItem,
+            this.visaToolStripMenuItem,
+            this.hjälpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(568, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arkivToolStripMenuItem
+            // 
+            this.arkivToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nyToolStripMenuItem});
+            this.arkivToolStripMenuItem.Name = "arkivToolStripMenuItem";
+            this.arkivToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.arkivToolStripMenuItem.Text = "Arkiv";
+            // 
+            // nyToolStripMenuItem
+            // 
+            this.nyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.närvaroToolStripMenuItem,
+            this.medlemToolStripMenuItem,
+            this.kontaktToolStripMenuItem});
+            this.nyToolStripMenuItem.Name = "nyToolStripMenuItem";
+            this.nyToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.nyToolStripMenuItem.Text = "Ny...";
+            // 
+            // närvaroToolStripMenuItem
+            // 
+            this.närvaroToolStripMenuItem.Name = "närvaroToolStripMenuItem";
+            this.närvaroToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.närvaroToolStripMenuItem.Text = "Närvaro";
+            // 
+            // medlemToolStripMenuItem
+            // 
+            this.medlemToolStripMenuItem.Name = "medlemToolStripMenuItem";
+            this.medlemToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.medlemToolStripMenuItem.Text = "Medlem";
+            // 
+            // kontaktToolStripMenuItem
+            // 
+            this.kontaktToolStripMenuItem.Name = "kontaktToolStripMenuItem";
+            this.kontaktToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.kontaktToolStripMenuItem.Text = "Kontakt";
+            // 
+            // redigeraToolStripMenuItem
+            // 
+            this.redigeraToolStripMenuItem.Name = "redigeraToolStripMenuItem";
+            this.redigeraToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.redigeraToolStripMenuItem.Text = "Redigera";
+            // 
+            // visaToolStripMenuItem
+            // 
+            this.visaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.närvaroToolStripMenuItem1,
+            this.medlemmarToolStripMenuItem});
+            this.visaToolStripMenuItem.Name = "visaToolStripMenuItem";
+            this.visaToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.visaToolStripMenuItem.Text = "Visa";
+            // 
+            // närvaroToolStripMenuItem1
+            // 
+            this.närvaroToolStripMenuItem1.Name = "närvaroToolStripMenuItem1";
+            this.närvaroToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.närvaroToolStripMenuItem1.Text = "Närvaro";
+            // 
+            // medlemmarToolStripMenuItem
+            // 
+            this.medlemmarToolStripMenuItem.Name = "medlemmarToolStripMenuItem";
+            this.medlemmarToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.medlemmarToolStripMenuItem.Text = "Medlemmar";
+            // 
+            // hjälpToolStripMenuItem
+            // 
+            this.hjälpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.visaHjälpToolStripMenuItem,
+            this.omToolStripMenuItem});
+            this.hjälpToolStripMenuItem.Name = "hjälpToolStripMenuItem";
+            this.hjälpToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.hjälpToolStripMenuItem.Text = "Hjälp";
+            // 
+            // visaHjälpToolStripMenuItem
+            // 
+            this.visaHjälpToolStripMenuItem.Name = "visaHjälpToolStripMenuItem";
+            this.visaHjälpToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.visaHjälpToolStripMenuItem.Text = "Visa hjälp";
+            // 
+            // omToolStripMenuItem
+            // 
+            this.omToolStripMenuItem.Name = "omToolStripMenuItem";
+            this.omToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.omToolStripMenuItem.Text = "Om";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 421);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arkivToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem närvaroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem medlemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kontaktToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redigeraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem visaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem närvaroToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem medlemmarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hjälpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem visaHjälpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem omToolStripMenuItem;
+    }
+}
+
