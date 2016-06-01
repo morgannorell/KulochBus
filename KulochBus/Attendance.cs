dd3e1080c20f5e49e422d37d0d961e04c25bd51e
morgannorell <mnorell@live.se>
2016-06-01 19:36:24 +0200

    Revert "uppdatering"
    This reverts commit 718ba9749c26e88ad64382d4f385a0ab12c7db38.

--- KulochBus/Attendance.cs
+++ KulochBus/Attendance.cs
@@ -159,14 +159,7 @@ namespace KulochBus
             Sql query = new Sql();
             DataTable dt = new DataTable();
 
-            //string sql = "SELECT * from attendance where date >= '" + date + "'";
-            
-
-            string sql = "SELECT a.date AS \"Datum\", tg.groupid AS \"Gruppid\", tg.name AS \"Gruppnamn\", " +
-                "COUNT(ma.memberid) AS \"Antal närvarande\" FROM memberattendance AS ma JOIN attendance AS " +
-                "a ON ma.attendanceid = a.attendanceid JOIN traininggroup AS tg ON tg.groupid = a.groupid " +
-                "GROUP BY ma.attendanceid, a.date, tg.name, tg.groupid where date >= '" + date + "'";
-
+            string sql = "SELECT * from attendance where date >= '" + date + "'";
             query.Select(sql);
 
             return dt;
